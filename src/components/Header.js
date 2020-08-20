bd09e4bd440ec210b2bcaceeab8668eae502d8e9
Ilkin <44848231+ilkinaslanli2001@users.noreply.github.com>
2020-08-20 17:18:43 +0400

    solve bug with image

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import   'materialize-css'
 import '../styles/header.css'
 import { Link } from "react-router-dom"
 import {useStateValue} from '../StateProvider'
-
+import logo from '../images/ILKMOV__logo.png'
 
 function Header()
 {
@@ -26,7 +26,7 @@ function Header()
     return(
         <ul className = "header">
             
-                <li className="header__item header__logo"><Link to='/'><img src='./images/ILKMOV__logo.png' /></Link></li>
+                <li className="header__item header__logo"><Link to='/'><img src={logo} /></Link></li>
                 
                 <li className="header__item browser-default header__login" >
                     {user ? <p><span>{user} <i class="material-icons">arrow_drop_down</i></span>
