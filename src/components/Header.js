bfb7dc58514eedf26aa54ea3a0846c32a22003a1
Ilkin <44848231+ilkinaslanli2001@users.noreply.github.com>
2020-08-21 23:26:20 +0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react"
 import   'materialize-css'
 import '../styles/header.css'
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import {useStateValue} from '../StateProvider'
 import logo from '../images/ILKMOV__logo.png'
 
@@ -12,14 +12,17 @@ function Header()
     
     
     const [{user}, dispatch] = useStateValue()
-    
+    const history = useHistory()
     const logoutUser=() =>
     {
         dispatch({
             type:'LOGOUT_USER'
         })
     }
-   
+   const onLogoClick = () =>{
+        localStorage.setItem('current_page_number',1)
+        history.push('/')
+   }
     
      
     
@@ -33,8 +36,7 @@ function Header()
                         <span className="header__hamburger-stick"></span>
                     </div>
                 </li>
-                <li className="header__item header__logo"><Link to='/'><img src={logo} /></Link></li>
-                
+                <li className="header__item "><img onClick={onLogoClick} className="header__logo" src={logo} /></li>
                 <li className="header__item browser-default header__login" >
                     {user ? <p className="header__username"><span>{user} <i class="material-icons">arrow_drop_down</i></span>
                        <ul className="header__dropdown">
